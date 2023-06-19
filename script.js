b53a9bbbf302518fd2076a4d949fab3047f1f5c4
Nitin Sharma <nitin.sharma969@gmail.com>
2023-06-19 19:55:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,44 +7,56 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function datediff(first, second) {        
+function datediff(first, second) {
     return Math.round((second - first) / (1000 * 60 * 60 * 24));
 }
 function parseDate(str) {
-    var mdy = str.split('/');
-    return new Date(mdy[2], mdy[0] - 1, mdy[1]);
+    var mdy = str.split('-');
+    return new Date(mdy[0], mdy[1] - 1, mdy[2]);
 }
+
+
 function myFunction() {
     const fromDate = document.getElementById('FromDateId').value;
     const toDate = document.getElementById('ToDateId').value;
     const count = document.getElementById('count').value;
-    const diffDays = Math.round(datediff(parseDate(fromDate), parseDate(toDate)) / count); 
+    const diffDays = Math.round(datediff(parseDate(fromDate), parseDate(toDate)) / count);
     let date = fromDate;
     for (let index = 0; index < count; index++) {
-        var characters = 'ABCD'
-        document.getElementById("billNo").value = characters.charAt(Math.floor(Math.random() * characters.length)) + (Math.floor(Math.random() * 1000) + 1000);
-        var hr = Math.floor(Math.random() * 18 + 5);
-        var min = Math.floor(Math.random() * 59);
+
+        const lowerVolRange = document.getElementById('lowerVolRangeId').value;
+        const upperVolRange = document.getElementById('upperVolRangeId').value;
+        const vol = getRandomFromRange(lowerVolRange, upperVolRange);
+        const lowerRateRange = document.getElementById('lowerRateRangeId').value;
+        const upperRateRange = document.getElementById('upperRateRangeId').value;
+        const rate = getRandomFromRange(lowerRateRange, upperRateRange);
+        const totalAmount = (vol * rate).toFixed(2);
+        const billNo = getRandomBillNo();
+        // const imageName = getRandomImageName();
+
+
+        document.getElementById("billNo").value = billNo;
+        // var hr = Math.floor(Math.random() * 18 + 5);
+        // var min = Math.floor(Math.random() * 59);
         var den = getRandomInt(729, 735) + "." + getRandomInt(0, 9) + "Kg/Cu.mtr";
-        document.getElementById("timeId").value = pad(hr, 2) + ":" + pad(min, 2);
-        document.getElementById("localVal").innerHTML = document.getElementById("localId").value;
+        // document.getElementById("timeId").value = pad(hr, 2) + ":" + pad(min, 2);
+        document.getElementById("localVal").innerHTML = generateRandomLocalId();
         document.getElementById("nozelVal").innerHTML = "0" + (Math.floor(Math.random() * 4) + 1);
         document.getElementById("dateVal").innerHTML = date;
-        document.getElementById("timeVal").innerHTML = document.getElementById("timeId").value;
-        document.getElementById("volVal").innerHTML = pad(document.getElementById("volId").value, 8);
-        document.getElementById("rateVal").innerHTML = document.getElementById("rateId").value;
-        var num = document.getElementById("volId").value * document.getElementById("rateId").value;
-        num = num.toFixed(2);
-        document.getElementById("amtVal").innerHTML = pad(num, 8);
-        document.querySelector("#billVal1").innerHTML = document.getElementById("billNo").value;
+        document.getElementById("timeVal").innerHTML = generateRandomTime();
+        document.getElementById("volVal").innerHTML = pad(vol, 8);
+        document.getElementById("rateVal").innerHTML = rate;
+
+        // document.getElementById('logo').setAttribute('src', imageName);
+
+        document.getElementById("amtVal").innerHTML = pad(totalAmount, 8);
+        document.querySelector("#billVal1").innerHTML = billNo;
         document.querySelector("#fipVal").innerHTML = pad(getRandomInt(1, 2), 2);
         // window.alert("done");
         date = new Date(date);
-        console.log(date);
         date.setDate(date.getDate() + diffDays);
-        console.log(date);
         date = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-        
+
         var divContents = document.getElementById("dvContainer").innerHTML;
         var printWindow = window.open('', '', 'height=400,width=800');
         printWindow.document.write('<html><head><title>DIV Contents</title>');
@@ -55,4 +67,76 @@ function myFunction() {
         printWindow.print();
         console.log("Printing done for ==================");
     }
+}
+function filldate() {
+    document.getElementById('FromDateId').value = document.getElementById('FromDateId').value.split('-').reverse().join("");
+}
+
+window.onload = function () {
+    // set fromdateId to last month date of today
+    document.getElementById('ToDateId').valueAsDate = new Date();
+    const dateInput = document.getElementById('FromDateId');
+    const currentDate = new Date();
+    currentDate.setMonth(currentDate.getMonth() - 1); // Go back one month
+    currentDate.setDate(20); // Set day to 20th
+    const formattedDate = currentDate.toISOString().substring(0, 10); // Format as YYYY-MM-DD
+    dateInput.value = formattedDate;
+
+}
+
+
+function generateRandomTime() {
+    const isMorning = Math.random() < 0.5; // Randomly select morning or night
+
+    let hours, minutes, period;
+    if (isMorning) {
+        // Morning time: between 9 AM and 11 AM
+        hours = Math.floor(Math.random() * 3) + 9; // Random hours between 9 and 11
+        period = 'AM';
+    } else {
+        // Night time: between 9 PM and 11:59 PM (midnight)
+        hours = Math.floor(Math.random() * 4) + 9; // Random hours between 9 and 12
+        period = 'PM';
+    }
+    minutes = Math.floor(Math.random() * 60); // Random minutes between 0 and 59
+
+    if (hours === 12) {
+        period = isMorning ? 'PM' : 'AM'; // Adjust period for 12 AM/PM
+    }
+
+    const formattedHours = hours % 12 || 12; // Convert to 12-hour format
+    const time = `${formattedHours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')} ${period}`;
+    return time;
+}
+
+function generateRandomLocalId() {
+    var characters = '0123456789';
+    var result = '';
+    for (var i = 0; i < 8; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+
+function getRandomBillNo() {
+    var characters = 'ABCD'
+    return characters.charAt(Math.floor(Math.random() * characters.length)) + (Math.floor(Math.random() * 1000) + 1000);
+}
+
+function getRandomFromRange(lowerRange, upperRange) {
+    console.log(lowerRange, upperRange);
+    var randomNum = Math.floor(Math.random() * (parseFloat(upperRange) - parseFloat(lowerRange) + 1)) + parseFloat(lowerRange);
+    console.log(randomNum);
+    return randomNum.toFixed(2);
+}
+
+function getRandomImageName() {
+    var images = [
+        'logo_1.png',
+        'logo.png',
+        'logo_w_2.png',
+        'logo_w.png',
+    ];
+    return images[Math.floor(Math.random() * images.length)];
 }
